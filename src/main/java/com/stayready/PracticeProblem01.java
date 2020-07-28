42bad42deee219b661767738027332c7ff9d2138
Jubril Aderomilehin <Jubril@Jubrils-MacBook-Air.local>
2020-07-28 12:34:29 -0400

    Im getting there

--- src/main/java/com/stayready/PracticeProblem01.java
+++ src/main/java/com/stayready/PracticeProblem01.java
@@ -1,4 +1,30 @@
 package com.stayready;
 
 public class PracticeProblem01 {
+
+    // this code checks if the code is a palindrone
+    // if it is return true, else return false
+    public boolean isPalindrone(String input01) {
+
+        // pointer pointing at  beggining
+        //pointer pointing at end
+        int i = 0, j= input01.length() -1;
+
+        //While you can still compare the charecters
+        while (i < j){
+
+            // if the char are not equal return false
+            if (input01.charAt(i) != input01.charAt(j))
+                return false;
+
+            //increment start position
+            // decrement from last position
+            i++;
+            j--;
+        }
+        return true;
+    }
+    public String getPalindrome( String input01){
+        return null;
+    }
 }
--- src/test/java/com/stayready/PracticeProblem01Test.java
+++ src/test/java/com/stayready/PracticeProblem01Test.java
@@ -1,4 +1,22 @@
 package com.stayready;
 
+import org.junit.Assert;
+import org.junit.Test;
+
 public class PracticeProblem01Test {
+    private static String input01 = "racecar";
+
+    @Test
+    public void isPalindroneTest(){
+        PracticeProblem01 practiceProblem01 = new PracticeProblem01();
+        Boolean actual = practiceProblem01.isPalindrone(input01);
+        Assert.assertTrue(actual);
+    }
+
+    public void getSubPalindronesTest(){
+        PracticeProblem01 practiceProblem01 = new PracticeProblem01();
+        String expected = "r a c e c a r";
+        String actual = practiceProblem01.getSubPalindromes(input01);
+        Assert.assertEquals(expected, actual);
+    }
 }
